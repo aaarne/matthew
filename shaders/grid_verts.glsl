1a84a3e93ad27a64b656ab71f86e114b239cd0fa
Arne Sachtler <arne.sachtler@gmail.com>
2019-11-18 20:14:17 +0100

    Set maximum grid shader intensity to white

--- shaders/grid_verts.glsl
+++ shaders/grid_verts.glsl
@@ -8,6 +8,6 @@ out vec4 vertColor;
 void main() {
     vec4 vpoint_mv = MV * vec4(position, 1.0);
     gl_Position = P * vpoint_mv;
-    vertColor = vec4(0.5, 0.5, 0.5, 1.0);
+    vertColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
 
