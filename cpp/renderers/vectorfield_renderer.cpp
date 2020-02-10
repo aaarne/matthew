14270954a92a41513ceef196c04ddfc254493c76
Arne Sachtler <arne.sachtler@gmail.com>
2020-02-10 22:39:59 +0100

    Add vectorfield renderer class

---
+++ cpp/renderers/vectorfield_renderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by arne on 2/10/20.
+//
+
+#include "vectorfield_renderer.h"
---
+++ cpp/renderers/vectorfield_renderer.h
@@ -0,0 +1,14 @@
+//
+// Created by arne on 2/10/20.
+//
+
+#ifndef MATTHEW_VECTORFIELD_RENDERER_H
+#define MATTHEW_VECTORFIELD_RENDERER_H
+
+
+class VectorfieldRenderer {
+
+};
+
+
+#endif //MATTHEW_VECTORFIELD_RENDERER_H
